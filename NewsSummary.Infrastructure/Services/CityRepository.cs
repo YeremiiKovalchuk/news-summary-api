5c28614dc9ed8bb7a7c699c19bc4ad9555edfcbc
Yeremii Kovalchuk <yeremii.kovalchuk@teamworkcommerce.com>
2024-10-04 10:07:22 +0200

    Final refactoring

--- NewsSummary.Infrastructure/Services/CityRepository.cs
+++ NewsSummary.Infrastructure/Services/CityRepository.cs
@@ -15,53 +15,49 @@ public class CityRepository : ICityRepository
         this._dbContext = summaryDBContext;
         this._logger = logger;
     }
-    public bool AddCity(CityDto cityInfo)
+
+    public async Task<bool> AddCityAsync(CityDto cityInfo)
     {
         this._logger.LogDebug($"Trying to add city with name {cityInfo.CityName} to Db.");
-        this._dbContext.Cities.Add(cityInfo);
-        if (this._dbContext.SaveChanges() > 0)
+        await this._dbContext.Cities.AddAsync(cityInfo);
+        if (await this._dbContext.SaveChangesAsync() > 0)
         {
             return true;
         }
         return false;   
     }
 
-    public List<CityDto?> GetAllCities()
+    public async Task<List<CityDto?>> GetAllCitiesAsync()
     {
         this._logger.LogDebug($"Trying to get all cities from Db.");
-        return _dbContext.Cities.AsNoTracking().ToList<CityDto?>();
+        return await _dbContext.Cities.AsNoTracking().ToListAsync<CityDto?>();
     }
 
-    public bool TryGetCityByName(string cityName, out CityDto? city)
+    public async Task<CityDto?> GetCityByName(string cityName)
     {
         this._logger.LogDebug($"Trying to get city with name {cityName} from Db");
-        city = _dbContext.Cities.AsNoTracking().FirstOrDefault(c => c.CityName == cityName);
-        if (city == null)
-        {
-            return false;
-        }
-        return true;
+        return await _dbContext.Cities.AsNoTracking().FirstOrDefaultAsync(c => c.CityName == cityName);
     }
 
-    public bool RemoveCity(string cityName)
+    public async Task<bool> RemoveCity(string cityName)
     {
         this._logger.LogDebug($"Trying to remove city with name {cityName} from Db");
-        var city = _dbContext.Cities.FirstOrDefault(c => c.CityName == cityName);
+        var city = await _dbContext.Cities.FirstOrDefaultAsync(c => c.CityName == cityName);
         if (city == null)
         { 
             return false;
         }
 
         this._dbContext.Cities.Remove(city);
-        this._dbContext.SaveChanges();
+        await this._dbContext.SaveChangesAsync();
         return true;
     }
 
-    public bool UpdateCity(CityDto cityInfo)
+    public async Task<bool> UpdateCity(CityDto cityInfo)
     {
         this._logger.LogDebug($"Trying to update city with name {cityInfo.CityName} in Db");
         this._dbContext.Cities.Update(cityInfo);
-        if (this._dbContext.SaveChanges() > 0)
+        if (await this._dbContext.SaveChangesAsync() > 0)
         {
             return true;
         }
